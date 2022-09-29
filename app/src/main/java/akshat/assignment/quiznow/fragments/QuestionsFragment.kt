6244d3ea315103609e16ca5cebbf1b3c4c2bc99c
Akshat Kumar Verma <akshatvermajbp@gmail.com>
2022-09-30 00:19:13 +0530

    Included API calls

--- app/src/main/java/akshat/assignment/quiznow/fragments/QuestionsFragment.kt
+++ app/src/main/java/akshat/assignment/quiznow/fragments/QuestionsFragment.kt
@@ -7,12 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import akshat.assignment.quiznow.R
 import akshat.assignment.quiznow.databinding.FragmentQuestionsBinding
+import akshat.assignment.quiznow.entities.Questions
+import android.widget.Toast
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 
 class QuestionsFragment : Fragment() {
 
     private var _binding: FragmentQuestionsBinding? = null
     private val binding get() = _binding!!
+
+    private val questions = mutableListOf<Questions>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,5 +32,45 @@ class QuestionsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        getData()
     }
+
+    private fun getData() {
+        val url = "https://b4e7d359-c58f-4aa3-a314-726b3baa3852.mock.pstmn.io/?quiz=true"
+        val queue = Volley.newRequestQueue(requireContext())
+
+        val jsonObjectRequest = object : JsonObjectRequest(
+            Method.GET, url, null,
+            {
+                val tempQuestions = it.getJSONObject("result").getJSONArray("questions")
+                for(i in 0 until tempQuestions.length()) {
+                    val jsonObject = tempQuestions.getJSONObject(i)
+                    val options = jsonObject.getJSONArray("options")
+                    questions.add(
+                        Questions(
+                            jsonObject.getString("lable"),
+                            options.getJSONObject(0).getString("lable"),
+                            options.getJSONObject(1).getString("lable"),
+                            options.getJSONObject(2).getString("lable"),
+                            options.getJSONObject(3).getString("lable")
+                        )
+                    )
+                }
+
+                binding.progressBar.visibility = View.GONE
+                binding.includeQuestions.questionsFormatLL.visibility = View.VISIBLE
+                binding.questionTextView.text = questions.toString()
+            },
+            {
+                Toast.makeText(requireContext(), "Some Error Occurred $it", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        ) {
+
+        }
+        queue.add(jsonObjectRequest)
+    }
+
+
+
 }
